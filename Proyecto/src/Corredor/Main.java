460e98506cd8195a5f2716f3e708d29f5a61140a
Nicole Angela Holguin Sancan <67934052+Nahs19@users.noreply.github.com>
2020-08-10 15:49:26 -0500

    Tarea_PC
    Programación Orientada a Objetos

--- Proyecto/src/Corredor/Main.java
+++ Proyecto/src/Corredor/Main.java
@@ -2,37 +2,36 @@ package Corredor;
 
 import java.util.Scanner;
 
+import Emisora.EmisoraFM;
+
 public class Main {
 
 			    public static void main(String[] args) {
 			    	
-			        int recarga;
+			        int recarga, opcion;
 			        Scanner in = new Scanner(System.in);
 			        Corredor corredor = new Corredor();
 				    System.out.print("Ingrese la energía del corredor actual: ");
-				    corredor.setEnergia(in.nextInt());		        
-			        
-			    
-			           System.out.println(" La persona se encuentra corriendo... ");			           
-			            	corredor.correr();			            
-			                corredor.verificarEnergia();
-		            	    corredor.energiaAgotada();
-			                System.out.print(" \n La energia del corredor es: "+ corredor.getEnergia()); 
-			            			            
-			            if(corredor.getEnergia() == 0){
-			                System.out.println("\n La energía se recargara...");			          
-			                	corredor.recargaEnergía(30);
-			                	corredor.entrenar();			                
-			                System.out.print("\n La energia del corredor es: "+ corredor.getEnergia());
+				    corredor.setEnergia(in.nextInt());		
+				    
+				    do {
+
+			            System.out.println("\n   Menu Principal   ");
+			            System.out.println("*********************");
+			            System.out.println("1. Recargar energía");
+			            System.out.println("2. Correr");
+			            System.out.println("3. Entrenar");
+			            System.out.println("4. Salir");
+			            System.out.println("Digite opcion (1/4): ");
+			            opcion = in.nextInt();
+			    			 
+			            switch(opcion){
+			                case 1: corredor.verificarEnergia();corredor.recargaEnergía(30);break;
+			                case 2: corredor.energiaAgotada();corredor.correr();break;
+			                case 3: corredor.verificarEnergia();corredor.entrenar();break;
+			                case 4: break;
+			                default: System.out.println("Opcion no válida");//Mensaje de dato erróneo
 			            }
-			           
-			            else{			            	
-			                System.out.println("\n La energía se recargara...");			                   
-			                	corredor.recargaEnergía(30);
-			                	corredor.entrenar();			                
-			                System.out.print("\n La energia del corredor es: "+ corredor.getEnergia());
-			            }
-			            
-			    }
-			    
-			}
+			        }while (opcion != 4);
+			      }
+}		    
